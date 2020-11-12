99bc4b4726423de4a82f729295c4795a408de3fb
Mike <watsonmichael919@gmail.com>
2020-11-12 16:40:04 -0600

    fixed code to work with latest versions of Marionette + Webpack

---
+++ webpack.config.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const path = require('path');
+const {merge} = require('webpack-merge');
+
+const webpackCommon = {
+  entry: {
+    app: ['./app/initialize']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: 'babel-loader',
+            options: {
+              presets: ["@babel/preset-env"]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.jst$/,
+        use: {
+          loader: 'underscore-template-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  output: {
+    filename: 'app.js',
+    path: path.join(__dirname, './public'),
+    publicPath: '/'
+  },
+  plugins: [
+    new MiniCssExtractPlugin({filename:'app.css'}),
+    new CopyWebpackPlugin({
+      patterns:[{
+        from: './app/assets/index.html',
+        to: './index.html'
+      }]
+    }),
+    new webpack.ProvidePlugin({
+      $: 'jquery',
+      _: 'underscore'
+    })
+  ],
+  resolve: {
+    modules: [
+      path.join(__dirname, './node_modules'),
+      path.join(__dirname, './app')
+    ]
+  },
+  resolveLoader: {
+    modules: [
+      path.join(__dirname, './node_modules')
+    ]
+  }
+};
+
+switch (process.env.npm_lifecycle_event) {
+  case 'start':
+  case 'dev':
+    module.exports = merge(webpackCommon, {
+      devtool: 'inline-source-map',
+      devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        compress: true,
+        port: 9000
+      }
+    });
+    break;
+  default:
+    module.exports = merge(webpackCommon, {
+      devtool: 'source-map'
+    });
+    break;
+}
